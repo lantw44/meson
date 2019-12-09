022e1dc9397403839dd2f2f1b9896e716403fbd5
Ting-Wei Lan <lantw@src.gnome.org>
2019-12-10 07:39:05 +0800

    Add options for overriding default pkg-config directories
    Since https://github.com/mesonbuild/meson/pull/4410 changes the
    installation directory of pkg-config files on FreeBSD, it is no longer
    possible for a portable script, such as JHBuild, to use the same
    directory layout on different platforms. Projects not using Meson's
    pkgconfig module are likely to keep installing pkg-config files into
    lib/pkgconfig, so we end up having both libdata/pkgconfig and
    lib/pkgconfig on FreeBSD. Unless JHBuild wants to patch build files for
    other build systems or manually move files after installation like what
    FreeBSD ports currently do, we will have a mixed directory layout when
    using a custom prefix on FreeBSD.
    
    Instead of hard-coding the path of pkg-config files, provide options
    for users to override the default values. JHBuild can just set
    '--libpkgconfigdir lib/pkgconfig --sharepkgconfigdir share/pkgconfig'
    to keep its existing behavior like what it already does to disable
    Debian-style multiarch with '--libdir lib'. Projects not using Meson's
    pkgconfig module are also encouraged to use these options to ensure a
    consistent directory layout.

--- mesonbuild/modules/pkgconfig.py
+++ mesonbuild/modules/pkgconfig.py
@@ -437,10 +437,7 @@ class PkgConfigModule(ExtensionModule):
         pcfile = filebase + '.pc'
         pkgroot = kwargs.get('install_dir', default_install_dir)
         if pkgroot is None:
-            if mesonlib.is_freebsd():
-                pkgroot = os.path.join(state.environment.coredata.get_builtin_option('prefix'), 'libdata', 'pkgconfig')
-            else:
-                pkgroot = os.path.join(state.environment.coredata.get_builtin_option('libdir'), 'pkgconfig')
+            pkgroot = state.environment.coredata.get_builtin_option('libpkgconfigdir')
         if not isinstance(pkgroot, str):
             raise mesonlib.MesonException('Install_dir must be a string.')
         self.generate_pkgconfig_file(state, deps, subdirs, name, description, url,
